4b85669fd0238276b0b383746397e72c8d35faaf
Yichun <59522685+xieyichun50@users.noreply.github.com>
2022-01-14 11:51:19 +0800

    Add files via upload

---
+++ 1ortholog_calling/orthofinder.sh
@@ -0,0 +1,5 @@
+orthofinder -f /home/yichun/huilab/snail/longest_protO -t 40 -a 40 -M msa -I 1.5
+
+#orthofinder -f /jelly_data/yichun/snail/longest_prot1 -t 22 -a 22 -M msa -I 1.8
+
+#orthofinder -f /jelly_data/yichun/snail/longest_prot1 -t 22 -a 22 -M msa -I 1.2
